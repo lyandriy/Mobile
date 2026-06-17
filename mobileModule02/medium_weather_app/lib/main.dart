e8086cb35efd2095bd9d85ed9868f56f6c747dc7
Lyudmyla <lyudmyla1829@gmail.com>
2026-06-17 18:19:47 +0200

    m02

---
+++ mobileModule02/medium_weather_app/lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppWeather());
+}
+
+class AppWeather extends StatelessWidget {
+  const AppWeather({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WeatherPage(),
+    );
+  }
+}
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> 
+      with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  final pages = [
+    "Currently",
+    "Today",
+    "Weekly",
+  ];
+
+  final TextEditingController _controller = TextEditingController();
+
+  String displayText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void useSearch() {
+    setState(() {
+      displayText = _controller.text;
+    });
+  }
+
+  void useGeo() {
+    setState(() {
+      displayText = "Geolocation";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: "Search city...",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: useSearch,
+            ),
+            IconButton(
+              icon: const Icon(Icons.my_location),
+              onPressed: useGeo,
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: pages.map((tab) {
+          return Center(
+            child: Text(
+              displayText.isEmpty
+                ? tab
+                  : "$tab - $displayText",
+              style: const TextStyle(fontSize: 24),
+            ),
+          );
+        },).toList(),
+      ),
+
+      bottomNavigationBar: BottomAppBar(
+        child: TabBar(
+          controller: _tabController,
+          tabs: const[
+            Tab(icon: Icon(Icons.cloud), text: "Currently"),
+            Tab(icon: Icon(Icons.today), text: "Today"),
+            Tab(icon: Icon(Icons.calendar_view_week), text: "Weekly"),
+          ],
+        ),
+      ),
+    );
+  }
+}
